88e7d0e2f3b9f92ba36c12250e577f8c5c1b4627
Elone Hoo <hi@elonehoo.me>
2023-01-16 19:18:54 +0800

    fix: error parsing fixes

--- demo/src/__output.js
+++ demo/src/__output.js
@@ -1,40 +1,20 @@
 'use strict';
 
-//# sourceMappingURL=__output.js.map
-+ 42;
+function foo(count) {
+  return count + 42;
 }
 
-describe("typescript test suite", () => {
-  it("tests the foo function", () => {
+describe("yet another ts test suite", () => {
+  it("tests the foo function again", () => {
     expect(foo(42)).to.equal(84);
   });
-  it("meows", () => {
+  it("another meows", () => {
     expect("meow").to.equal("meow");
   });
-  it("test a function", () => {
+  it("this is a function spy", () => {
     const spy = sinon.fake();
     spy();
     expect(spy.callCount).to.equal(1);
   });
-  it("error", () => {
-    expect(1).to.equal(2);
-  });
-  it("wait for async op", async () => {
-    await wait(1e3);
-    expect(0).to.equal(0);
-  });
-  it("wait for async op 2", async () => {
-    await wait(1e3);
-    expect(0).to.equal(0);
-  });
-  it("wait for async op 3", async () => {
-    await wait(1e3);
-    expect(0).to.equal(0);
-  });
 });
-function wait(delay) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  });
-}
 //# sourceMappingURL=__output.js.map
