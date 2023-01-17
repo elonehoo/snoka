a4f0cb567b39cf7983a5be77bebfec9011c2aa94
elonehoo <hi@elonehoo.me>
2023-01-17 22:54:48 +0800

    test: more runner tests

--- tests/specs/reactive/__output.js
+++ tests/specs/reactive/__output.js
@@ -4,36 +4,32 @@ var reactive = require('@snoka/reactive');
 var path = require('path');
 
 let fs;
-describe("reactive fs: file content", () => {
+describe("reactive fs: list()", () => {
   beforeEach(async () => {
     fs = await reactive.createReactiveFileSystem({
-      baseDir: path.join(__dirname, "content-example"),
+      baseDir: path.join(__dirname, "list-example"),
       glob: ["**/*.js"]
     });
   });
-  test("initial content is undefined", () => {
-    expect(fs.files["meow.js"].content).to.be.undefined();
+  test("scan for files", () => {
+    expect(fs.list().sort()).to.eql(["sub/waf.js", "meow.js"].sort());
   });
-  test("waitForContent", async () => {
-    expect(await fs.files["meow.js"].waitForContent).to.eql("console.log('meow')\n");
+  test("exclude sub directories", () => {
+    expect(fs.list(".", { excludeSubDirectories: true }).sort()).to.eql(["meow.js"].sort());
   });
-  test("reactive content (initial load)", async () => {
-    let result;
+  test("list sub directory", () => {
+    expect(fs.list("sub").sort()).to.eql(["sub/waf.js"].sort());
+  });
+  test("list is reactive", () => {
+    let results = [];
     fs.effect(() => {
-      result = fs.files["meow.js"].content;
+      results = fs.list(".", { excludeSubDirectories: true }).sort();
     });
-    expect(result).to.be.undefined();
-    await peeky.retry(() => expect(result).to.eql("console.log('meow')\n"));
-  });
-  test("reactive content", async () => {
-    await fs.files["meow.js"].waitForContent;
-    let result;
-    fs.effect(() => {
-      result = fs.files["meow.js"].content;
-    });
-    expect(result).to.eql("console.log('meow')\n");
-    fs.files["meow.js"].content = "waf";
-    await peeky.retry(() => expect(result).to.eql("waf"));
+    expect(results).to.eql(["meow.js"].sort());
+    fs.createFile("foo.js", "");
+    expect(results).to.eql(["meow.js", "foo.js"].sort());
+    fs.files["foo.js"].remove();
+    expect(results).to.eql(["meow.js"].sort());
   });
 });
 //# sourceMappingURL=__output.js.map
