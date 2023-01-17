526f687d260ad89c17d805064c6a17bd406868d0
elonehoo <hi@elonehoo.me>
2023-01-17 22:52:05 +0800

    feat: handle build or exec errors

---
+++ packages/runner/src/global/index.ts
@@ -0,0 +1,10 @@
+import { Context } from '../types'
+import { retry } from './test-flow'
+
+export function createSnokaGlobal (ctx: Context) {
+  return {
+    retry,
+  }
+}
+
+export type SnokaGlobal = ReturnType<typeof createSnokaGlobal>
